c1b8f418ca750232ec53dc708900bd8eb3f33fbf
Sergjj Derkach <sergjj-derkach@rambler.ru>
2020-04-03 12:18:37 +0300

    hw-28

--- hw-14/js/script.js
+++ hw-14/js/script.js
@@ -1,18 +1,19 @@
 let list = [];
 let ulList = document.getElementById('getList');
 
-fetch('https://jsonplaceholder.typicode.com/photos?_limit=50')
+fetch('https://jsonplaceholder.typicode.com/todos')
 
 .then((response) => {
     return response.json()
 })
 .then(data =>{
     list = data;
+    console.log(list);
     list.forEach(function(item) {
         let li = document.createElement('li');
-        let elText = document.createTextNode(item);
+        let elText = document.createTextNode(item.title);
         li.appendChild(elText);
+        li.classList.add(item.completed);
         ulList.appendChild(li);
     });
-
 });
---
+++ hw-28/src/app.js
@@ -0,0 +1,40 @@
+import './styles.css';
+
+const ws = new WebSocket('wss://fep-app.herokuapp.com/');
+let nameUser = document.getElementById('nameUser');
+let messageUser = document.getElementById('messageUser');
+const onlineChat = document.getElementById('onlineChat');
+const listMessages = document.getElementById('listMessages');
+
+
+onlineChat.addEventListener('click', onAddMessage);
+ws.onmessage = onMessage;
+
+function onMessage(message){
+    let messageEvent = JSON.parse(message.data);
+    renderMessage(messageEvent);  
+    
+}
+
+function renderMessage(messageEvent){
+    let span = document.createElement('span');
+    span.innerHTML = `<b>${messageEvent.payload.author}</b>: <em>${messageEvent.payload.message}</em>`;
+    listMessages.appendChild(span);
+}
+function onAddMessage(e){
+    e.preventDefault();
+    if(e.target.classList.contains('addBtn')){
+        ws.send(JSON.stringify({
+            action:'message',
+            payload: {
+                author: nameUser.value,
+                message: messageUser.value
+            }
+        })); 
+    }
+    clearMessage();
+}
+
+function clearMessage(){
+    messageUser.value = '';
+}
